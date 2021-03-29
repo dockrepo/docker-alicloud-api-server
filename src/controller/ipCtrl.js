14dc03d97e3c3bb6325e8edcd8730b54cab184f5
Zyao89 <zyao89@gmail.com>
2021-03-29 16:57:04 +0800

    feat: 增加 ip 查询和展示

---
+++ src/controller/ipCtrl.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const Joi = require('joi');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const HTML_CONTENT = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ SERVER_NAME }}</title>
+</head>
+<body>{{ CONTENT }}</body>
+</html>
+`;
+
+class IPUtils {
+    constructor(info) {
+        this.info = info || {};
+    }
+
+    async GetIPSrc() {
+        const url = 'https://www.ip138.com/';
+        const { data: html } = await axios.get(url);
+        const $ = cheerio.load(html);
+        const iframe = $('iframe').first();
+        const src = iframe.attr('src');
+        return !/^\/\//.test(src) ? src : `http:${src}`;
+    }
+
+    async GetIP() {
+        const url = await this.GetIPSrc();
+        const { data: html } = await axios.get(url);
+        const $ = cheerio.load(html);
+        return $('p>a').first().text();
+    }
+
+    async Index() {
+        const url = await this.GetIPSrc();
+        const { data: html } = await axios.get(url);
+        const $ = cheerio.load(html);
+        const $p = $('p').first();
+        $p.find('a').first().replaceWith(`<span style="color: green;">${$p.find('a').text()}</span>`);
+        const body = $p.html().trim();
+        return HTML_CONTENT
+            .replace(/{{\s*SERVER_NAME\s*}}/igm, this.info.name)
+            .replace(/{{\s*CONTENT\s*}}/igm, `<div style="text-align: center;"><h1>iP地址归属地查询</h1><p>${body}</p></div>`);
+    }
+}
+
+module.exports = function aliyunCtrl(app) {
+    const router = app.$newRouter();
+    const config = app.$config || {};
+
+    router.get('/', async (ctx, next) => {
+        try {
+            const action = new IPUtils(config.info);
+            const result = await action.Index();
+            ctx.body = result;
+        } catch (e) {
+            ctx.error(e);
+        }
+    });
+
+    router.get('/ip', async (ctx, next) => {
+        try {
+            const action = new IPUtils(config.info);
+            const result = await action.GetIP();
+            ctx.result(result);
+        } catch (e) {
+            ctx.error(e);
+        }
+    });
+
+    return router;
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@
 
 module.exports = function(app) {
     console.info(__filename);
+    app.useRoute(require('@/controller/ipCtrl')(app));
     app.useRoute('/api/aliyun', require('@/controller/alicloudCtrl')(app));
 };
